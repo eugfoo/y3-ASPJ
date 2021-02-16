ce41d8e91683e6f681690cf362957b55f2036e36
Eugene <184707d@mymail.nyp.edu.sg>
2021-02-16 11:11:22 +0800

    added kick condition into kovi's new page

--- FinalProj/FinalProj/RequestAccess.aspx.cs
+++ FinalProj/FinalProj/RequestAccess.aspx.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using FinalProj.BLL;
+using Newtonsoft.Json.Linq;
 
 namespace FinalProj
 {
@@ -12,6 +15,7 @@ namespace FinalProj
     {
         Admins ad = new Admins();
         RequestPermission req = new RequestPermission();
+        protected Sessionmg sesDeets;
 
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -33,20 +37,33 @@ namespace FinalProj
                     }
                     else
                     {
-                        adminDiv.Visible = false;
-                        ad = ad.GetAllAdminWithEmail(Session["subadminEmail"].ToString());
-                        lblCurrentRole.Text = ad.adminRole;
-                        listReq = req.getAllRequestsEmail(ad.adminEmail);
-                        btnRequest.Enabled = false;
-                        foreach (var i in listReq)
+                        Sessionmg ses = new Sessionmg();
+
+                        sesDeets = ses.GetSession(Session["subadminEmail"].ToString());
+                        if (sesDeets.Active == 1)
                         {
-                            if (i.subAdminEmail == ad.adminEmail)
+                            adminDiv.Visible = false;
+                            ad = ad.GetAllAdminWithEmail(Session["subadminEmail"].ToString());
+                            lblCurrentRole.Text = ad.adminRole;
+                            listReq = req.getAllRequestsEmail(ad.adminEmail);
+                            btnRequest.Enabled = false;
+                            foreach (var i in listReq)
                             {
-                                lblSuccess.Visible = true;
-                                lblSuccess.Text = "Your request has been sent for review";
-                                btnRequest.Enabled = false;
-                                roleDDL.Enabled = false;
+                                if (i.subAdminEmail == ad.adminEmail)
+                                {
+                                    lblSuccess.Visible = true;
+                                    lblSuccess.Text = "Your request has been sent for review";
+                                    btnRequest.Enabled = false;
+                                    roleDDL.Enabled = false;
+                                }
                             }
+                        }
+                        else
+                        {
+
+                            Session.Clear();
+                            string err = "SessionKicked";
+                            Response.Redirect("homepage.aspx?error=" + err);
                         }
                     }
                 }
@@ -83,9 +100,18 @@ namespace FinalProj
             else
             {
                 ad = ad.GetAllAdminWithEmail(Session["subadminEmail"].ToString());
+                adminLog adl = new adminLog();
+                MainAdmins mad = new MainAdmins();
+                Users us = new Users();
 
+                string ipAddr = GetIPAddress();
+                string countryLogged = CityStateCountByIp(ipAddr);
+                roles rl = new roles();
+                DateTime dt = DateTime.Now;
                 RequestPermission request = new RequestPermission();
                 request.AddRequest(ad.adminEmail, roleDDL.SelectedItem.Text, ad.adminRole);
+                adl.AddAdminLog(dt, ad.adminName, ipAddr, "Requested for " + roleDDL.SelectedItem.Text + " permission", "-", ad.adminEmail, countryLogged);
+
                 Response.Redirect("/RequestAccess.aspx");
             }
 
@@ -98,10 +124,65 @@ namespace FinalProj
 
             string email = row.Cells[0].Text;
             string role = row.Cells[1].Text;
+            adminLog adl = new adminLog();
 
+            string ipAddr = GetIPAddress();
+            string countryLogged = CityStateCountByIp(ipAddr);
+            DateTime dt = DateTime.Now;
+            adl.AddAdminLog(dt, "Admin", ipAddr, "Accepted request for " + role + " permission made by " + email, "-", Session["adminEmail"].ToString(), countryLogged); ;
             ad.UpdateRoleByEmail(email, role);
             req.DeleteByIdEmail(email);
             Response.Redirect("/RequestAccess.aspx");
+        }
+        public static string CityStateCountByIp(string IP)
+        {
+            //var url = "http://freegeoip.net/json/" + IP;
+            //var url = "http://freegeoip.net/json/" + IP;
+            string url = "http://api.ipstack.com/" + IP + "?access_key=01ca9062c54c48ef1b7d695b008cae00";
+            var request = System.Net.WebRequest.Create(url);
+            WebResponse myWebResponse = request.GetResponse();
+            Stream stream = myWebResponse.GetResponseStream();
+
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                string json = reader.ReadToEnd();
+                JObject objJson = JObject.Parse(json);
+                string Country = objJson["country_name"].ToString();
+                string Country_code = objJson["country_code"].ToString();
+                if (Country == "")
+                {
+                    Country = "-";
+                }
+
+                else if (Country_code == "")
+                {
+                    Country = Country;
+                }
+                else
+                {
+                    Country = Country + " (" + Country_code + ")";
+                }
+                return Country;
+
+            }
+
+        }
+        // Get IP Address of user
+        protected string GetIPAddress()
+        {
+            System.Web.HttpContext context = System.Web.HttpContext.Current;
+            string ipAddress = context.Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
+
+            if (!string.IsNullOrEmpty(ipAddress))
+            {
+                string[] addresses = ipAddress.Split(',');
+                if (addresses.Length != 0)
+                {
+                    return addresses[0];
+                }
+            }
+
+            return context.Request.ServerVariables["REMOTE_ADDR"];
         }
     }
 }
