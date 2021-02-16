d7a1a857e479d85cfae9c057364061574888b507
Eugene <184707d@mymail.nyp.edu.sg>
2021-02-16 18:04:41 +0800

    test

--- FinalProj/FinalProj/Global.asax.cs
+++ FinalProj/FinalProj/Global.asax.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 using System.Web.Security;
 using System.Web.SessionState;
@@ -14,6 +16,57 @@ namespace FinalProj
     public class Global : System.Web.HttpApplication
     {
         List<Logs> lgList;
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            //Check If it is a new session or not , if not then do the further checks
+            if (Request.Cookies["ASP.NET_SessionId"] != null && Request.Cookies["ASP.NET_SessionId"].Value != null)
+            {
+                string newSessionID = Request.Cookies["ASP.NET_SessionID"].Value;
+                //Check the valid length of your Generated Session ID
+                if (newSessionID.Length <= 24)
+                {
+                    //Log the attack details here
+                    Response.Cookies["TriedTohack"].Value = "True";
+                    //throw new HttpException("Invalid Request");
+                }
+
+                //Genrate Hash key for this User,Browser and machine and match with the Entered NewSessionID
+                if (GenerateHashKey() != newSessionID.Substring(24))
+                {
+                    //Log the attack details here
+                    Response.Cookies["TriedTohack"].Value = "True";
+                    throw new HttpException("Invalid Request");
+                }
+
+                //Use the default one so application will work as usual//ASP.NET_SessionId
+                Request.Cookies["ASP.NET_SessionId"].Value = Request.Cookies["ASP.NET_SessionId"].Value.Substring(0, 24);
+            }
+
+        }
+        private string GenerateHashKey()
+        {
+            StringBuilder myStr = new StringBuilder();
+            myStr.Append(Request.Browser.Browser);
+            myStr.Append(Request.Browser.Platform);
+            myStr.Append(Request.Browser.MajorVersion);
+            myStr.Append(Request.Browser.MinorVersion);
+            //myStr.Append(Request.LogonUserIdentity.User.Value);
+            SHA1 sha = new SHA1CryptoServiceProvider();
+            byte[] hashdata = sha.ComputeHash(Encoding.UTF8.GetBytes(myStr.ToString()));
+            return Convert.ToBase64String(hashdata);
+        }
+
+        protected void Application_EndRequest(object sender, EventArgs e)
+        {
+            //Pass the custom Session ID to the browser.
+            if (Response.Cookies["ASP.NET_SessionId"] != null)
+            {
+                Response.Cookies["ASP.NET_SessionId"].Value = Request.Cookies["ASP.NET_SessionId"].Value + GenerateHashKey();
+            }
+
+        }
+
         void Application_Start(object sender, EventArgs e)
         {
             RouteConfig.RegisterRoutes(System.Web.Routing.RouteTable.Routes);
